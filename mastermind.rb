3bd0a20238d5bd3f37361b73941bdd4ff2434352
J-Rozas <jrozaslima@gmail.com>
2021-01-01 19:44:21 +0000

    Inmplemented the algorithm that allows the computer guess the user's code. Corrected several bugs. Styled the file a bit.

--- mastermind.rb
+++ mastermind.rb
@@ -1,100 +1,103 @@
+# frozen_string_literal: false
+
+# Module that holds the methods to run the game
 module UserComputer
   def number_to_color(num)
     case num
     when 0
-      "r"
+      'r'
     when 1
-      "b"
+      'b'
     when 2
-      "y"
+      'y'
     when 3
-      "g"
+      'g'
     when 4
-      "c"
+      'c'
     when 5
-      "w"
+      'w'
     end
   end
 
   def user_code
     possibilities = (1111..6666).to_a
-    possibilities.map! { |el| el.to_s}
-    possibilities.select! do |el| 
-      !el.include?("7") && !el.include?("8") && !el.include?("9") && !el.include?("0")
+    possibilities.map!(&:to_s)
+    possibilities.select! do |el|
+      !el.include?('7') && !el.include?('8') && !el.include?('9') && !el.include?('0')
     end
-    possibilities.map! { |el| el.split("")}
-    puts "Set the code that the computer is going to guess."
-    code = gets.chomp!.split("").freeze
+    possibilities.map! { |el| el.split('') }
+    puts 'Set the code that the computer is going to guess.'
+    code = gets.chomp!.split('').freeze
     puts "Your code is: #{code}"
-    computer_guess = ["1", "1", "2", "2"]
-    num_run = 0
-    while num_run != 12
-      possibilities_helper = []
-      answer = Array.new
+    computer_guess = %w[1 1 2 2]
+    rounds_to_solve = 1
+    12.times do
+      answer = []
       code_helper = code.dup
+      guess_copy = computer_guess.dup
+      code_helper.each_with_index.map do |element, index|
+        next unless element == guess_copy[index]
+
+        answer.push('O')
+        guess_copy[index] = 'X'
+        code_helper[index] = 'n'
+      end
       code_helper.each_with_index do |element, index|
-        if element == computer_guess[index]
-          answer.push("O")
-          code_helper[index] = "X"
-        elsif code_helper.include?(computer_guess[index])
-          answer.push("o")          
-        end
+        next unless guess_copy.include?(element)
+
+        answer.push('o')
+        hold_index = guess_copy.index(element)
+        guess_copy[hold_index] = 'X'
+        code_helper[index] = 'n'
       end
       puts "The computer's guess is #{computer_guess}."
-      p answer
-      if answer == ["O", "O", "O", "O"]
-        puts "The computer wins!"
+      puts "The answer is #{answer}"
+      if answer == %w[O O O O]
+        puts "The computer cracked your code in #{rounds_to_solve} rounds!"
         break
       else
         puts "The computer didn't quite get it yet..."
-      end
-=begin
-      possibilities.each do |elthing|
-        holder_answer = Array.new
-        code_helper = code.dup
-        code_helper.each_with_index do |element, index|
-          if element == elthing[index]
-            holder_answer.push("O")
-            code_helper[index] = "X"
-          elsif code_helper.include?(elthing[index])
-            holder_answer.push("o")
-          end
-        end
-        if !holder_answer.sort.join.include?(answer.sort.join)
-          possibilities.delete(elthing)
-        end
-      end
-      computer_guess = possibilities[0]
-      num_run += 1
-      p num_run
-      p possibilities
-    end
-=end
-  end
-=begin
-    p answer
-    computer_guess = computer_guess.join("")
-    p computer_guess
-    p possibilities[0]
-    copy_holder = []
-    possibilities.each do |element|
-      if element != computer_guess
-        copy_holder.push(element)
-      end
-    end
-    possibilities -= copy_holder
-    p possibilities
-=end
+        puts
+        print 'Press any key to continue into the next round'
+        gets
+        possibilities_helper = []
+        first_pos_holder = possibilities.dup
+        first_pos_holder.each do |el|
+          el_holder = el.dup
+          holder_answer = []
+          guess_help = computer_guess.dup
+          guess_help.each_with_index.map do |element, index|
+            next unless element == el_holder[index]
 
-=begin
+            holder_answer.push('O')
+            guess_help[index] = 'X'
+            el_holder[index] = 'n'
+          end
+          guess_help.each_with_index do |element, _index|
+            next unless el_holder.include?(element)
+
+            holder_answer.push('o')
+            hold_index = el_holder.index(element)
+            el_holder[hold_index] = 'n'
+          end
+          possibilities_helper.push(el) if holder_answer.sort! == answer.sort!
+        end
+        possibilities = possibilities_helper.dup
+        computer_guess = possibilities[0]
+      end
+      rounds_to_solve += 1
+    end
+  end
+
   def computer_code
-    code = Array.new
-    4.times {code.push(number_to_color(rand(6)))}
+    code = []
+    4.times { code.push(number_to_color(rand(6))) }
     code.freeze
-    holder = 2
+    round_num = 1
     p code
     12.times do
-      user_guess = Array.new
+      puts "Round #{round_num}"
+      user_guess = []
       num_user_guess = 1
       4.times do
         print "Make your number #{num_user_guess} guess: "
@@ -104,42 +107,52 @@ module UserComputer
       end
       puts "Your guess is #{user_guess}, and..."
       if user_guess == code
-        puts "You win!"
+        puts "You win! You craked the code in #{round_num} rounds."
         break
       else
         puts "You didn't quite get it just yet, but the computer says..."
-        answer = Array.new
-        code.each_with_index do |element, index|
-          if element == user_guess[index]
-            answer.push("Right color, right position")
-          elsif code.include?(user_guess[index])
-            answer.push("Right color, wrong position")
-          else
-            answer.push("Not the right color")
-          end
+        answer = []
+        code_helper = code.dup
+        guess_copy = user_guess.dup
+        code_helper.each_with_index.map do |element, index|
+          next unless element == guess_copy[index]
+
+          answer.push('O')
+          guess_copy[index] = 'X'
+          code_helper[index] = 'n'
         end
-        puts answer.shuffle!
+        code_helper.each_with_index do |element, index|
+          next unless guess_copy.include?(element)
+
+          answer.push('o')
+          hold_index = guess_copy.index(element)
+          guess_copy[hold_index] = 'X'
+          code_helper[index] = 'n'
+        end
+        p answer.shuffle!
         puts
       end
+      round_num += 1
+      puts "You haven't been able to crack the code. :-(\nThe original code was #{code}." if round_num == 13
     end
   end
-=end
 end
 
+# class that starts the game
 class Game
   extend UserComputer
-  
+
   def self.start_game
-    puts "Do you want to set the code (s) or guess (g) the code from the computer?:"
+    puts 'Do you want to set the code (s) or guess (g) the code from the computer?:'
     response = gets.chomp!.downcase
-    unless response == "s" || response == "g"
+    unless %w[s g].include?(response)
       puts "Please, select either 's' (you set the code) or 'g' (the computer sets the code and you guess it):"
       response = gets.chomp!.downcase
     end
-    if response == "s"
-      user_code()
+    if response == 's'
+      user_code
     else
-      computer_code()
+      computer_code
     end
   end
 end
