13d55ddd0b62cd01ca7ced002d615e1b0540bdbf
J-Rozas <jrozaslima@gmail.com>
2021-01-01 02:09:16 +0000

    Put back the function that I deleted earlier because it is actually necessary.

--- mastermind.rb
+++ mastermind.rb
@@ -1,22 +1,77 @@
 module UserComputer
-  
+  def number_to_color(num)
+    case num
+    when 0
+      "r"
+    when 1
+      "b"
+    when 2
+      "y"
+    when 3
+      "g"
+    when 4
+      "c"
+    when 5
+      "w"
+    end
+  end
+
   def user_code
     possibilities = (1111..6666).to_a
     possibilities.map! { |el| el.to_s}
+    possibilities.select! do |el| 
+      !el.include?("7") && !el.include?("8") && !el.include?("9") && !el.include?("0")
+    end
+    possibilities.map! { |el| el.split("")}
     puts "Set the code that the computer is going to guess."
     code = gets.chomp!.split("").freeze
     puts "Your code is: #{code}"
     computer_guess = ["1", "1", "2", "2"]
-    answer = Array.new
-    code.each_with_index do |element, index|
-      if element == computer_guess[index]
-        answer.push("Right color, right position")
-      elsif code.include?(computer_guess[index])
-        answer.push("Right color, wrong position")
-      else
-        answer.push("Not the right color")
+    num_run = 0
+    while num_run != 12
+      possibilities_helper = []
+      answer = Array.new
+      code_helper = code.dup
+      code_helper.each_with_index do |element, index|
+        if element == computer_guess[index]
+          answer.push("O")
+          code_helper[index] = "X"
+        elsif code_helper.include?(computer_guess[index])
+          answer.push("o")          
+        end
       end
+      puts "The computer's guess is #{computer_guess}."
+      p answer
+      if answer == ["O", "O", "O", "O"]
+        puts "The computer wins!"
+        break
+      else
+        puts "The computer didn't quite get it yet..."
+      end
+=begin
+      possibilities.each do |elthing|
+        holder_answer = Array.new
+        code_helper = code.dup
+        code_helper.each_with_index do |element, index|
+          if element == elthing[index]
+            holder_answer.push("O")
+            code_helper[index] = "X"
+          elsif code_helper.include?(elthing[index])
+            holder_answer.push("o")
+          end
+        end
+        if !holder_answer.sort.join.include?(answer.sort.join)
+          possibilities.delete(elthing)
+        end
+      end
+      computer_guess = possibilities[0]
+      num_run += 1
+      p num_run
+      p possibilities
     end
+=end
+  end
+=begin
     p answer
     computer_guess = computer_guess.join("")
     p computer_guess
@@ -29,31 +84,8 @@ module UserComputer
     end
     possibilities -= copy_holder
     p possibilities
-  end
-=begin
-  def user_code
-
-    possibilities = [1,2,3,4,5,6]
-
-    puts "Set the code that the computer is going to guess."
-    code = gets.chomp!.split("").freeze
-    puts "Your code is: #{code}"
-    computer_guess = []
-    num_tries = 0
-    while computer_guess != code
-      computer_guess.shift(4)
-      4.times {computer_guess.push(possibilities.sample(1)).flatten!}
-      p computer_guess
-      if code - computer_guess == computer_guess
-        possibilities = computer_guess
-        p possibilities
-      end
-      num_tries += 1
-    end
-    p computer_guess
-    p num_tries
-  end
 =end
+
 =begin
   def computer_code
     code = Array.new
